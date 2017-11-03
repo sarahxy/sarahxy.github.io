ca44646c053bd48d8326f09788755667e6d55177
Sarah <s-huang@berkeley.edu>
2017-11-03 12:35:05 -0700

    reduced image size

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,49 +29,89 @@ $(document).ready(function() {
 
 	// listeners for the carousel-navigator
 	$('#syncfab-about').on({ 'click': function() {
-		$('.carousel-container img').attr('src', 'assets/img/syncfab/about.png');
+		$('.carousel-container img').attr('src', 'assets/img/syncfab/about.jpg');
+		$('#syncfab-page').text("ABOUT US");
+	}
+	});
+
+	$('#syncfab-about').on({ 'hover': function() {
 		$('#syncfab-page').text("ABOUT US");
 	}
 	});
 
 	$('#syncfab-contact').on({ 'click': function() {
-		$('.carousel-container img').attr('src', 'assets/img/syncfab/contact.png');
+		$('.carousel-container img').attr('src', 'assets/img/syncfab/contact.jpg');
+		$('#syncfab-page').text("CONTACT US");
+	}
+	});
+
+	$('#syncfab-contact').on({ 'hover': function() {
 		$('#syncfab-page').text("CONTACT US");
 	}
 	});
 
 	$('#syncfab-faqs').on({ 'click': function() {
-		$('.carousel-container img').attr('src', 'assets/img/syncfab/faqs.png');
+		$('.carousel-container img').attr('src', 'assets/img/syncfab/faqs.jpg');
+		$('#syncfab-page').text("FAQs");
+	}
+	});
+
+	$('#syncfab-faqs').on({ 'hover': function() {
 		$('#syncfab-page').text("FAQs");
 	}
 	});
 
 	$('#syncfab-press').on({ 'click': function() {
-		$('.carousel-container img').attr('src', 'assets/img/syncfab/press.png');
+		$('.carousel-container img').attr('src', 'assets/img/syncfab/press.jpg');
+		$('#syncfab-page').text("PRESS");
+	}
+	});
+
+	$('#syncfab-press').on({ 'hover': function() {
 		$('#syncfab-page').text("PRESS");
 	}
 	});
 
 	$('#syncfab-buyer').on({ 'click': function() {
-		$('.carousel-container img').attr('src', 'assets/img/syncfab/howitworksbuyer.png');
+		$('.carousel-container img').attr('src', 'assets/img/syncfab/howitworksbuyer.jpg');
+		$('#syncfab-page').text("HOW IT WORKS (BUYERS)");
+	}
+	});
+
+	$('#syncfab-buyer').on({ 'hover': function() {
 		$('#syncfab-page').text("HOW IT WORKS (BUYERS)");
 	}
 	});
 
 	$('#syncfab-factory').on({ 'click': function() {
-		$('.carousel-container img').attr('src', 'assets/img/syncfab/howitworksfactory.png');
+		$('.carousel-container img').attr('src', 'assets/img/syncfab/howitworksfactory.jpg');
+		$('#syncfab-page').text("HOW IT WORKS (FACTORIES)");
+	}
+	});
+
+	$('#syncfab-factory').on({ 'hover': function() {
 		$('#syncfab-page').text("HOW IT WORKS (FACTORIES)");
 	}
 	});
 
 	$('#syncfab-blog').on({ 'click': function() {
-		$('.carousel-container img').attr('src', 'assets/img/syncfab/bloghome.png');
+		$('.carousel-container img').attr('src', 'assets/img/syncfab/bloghome.jpg');
+		$('#syncfab-page').text("BLOG HOME");
+	}
+	});
+
+	$('#syncfab-blog').on({ 'hover': function() {
 		$('#syncfab-page').text("BLOG HOME");
 	}
 	});
 
 	$('#syncfab-blogpost').on({ 'click': function() {
-		$('.carousel-container img').attr('src', 'assets/img/syncfab/blogpost.png');
+		$('.carousel-container img').attr('src', 'assets/img/syncfab/blogpost.jpg');
+		$('#syncfab-page').text("SAMPLE BLOG POST");
+	}
+	});
+
+	$('#syncfab-blogpost').on({ 'hover': function() {
 		$('#syncfab-page').text("SAMPLE BLOG POST");
 	}
 	});
